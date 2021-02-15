904334885c36981527441f2896d68a701e4d888c
municipal <municipal@desktop>
2021-02-15 15:52:13 -0300

    Instalado com sucesso na sua máquina: desktop!

--- gitcam.sh
+++ gitcam.sh
@@ -89,3 +89,4 @@ done
 # Instalado em seg fev 15 15:44:21 -03 2021
 # Instalado em seg fev 15 15:46:59 -03 2021
 # Instalado em seg fev 15 15:52:00 -03 2021
+# Instalado em seg fev 15 15:52:13 -03 2021
