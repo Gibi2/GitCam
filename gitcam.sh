5bfd985ac9cb4a621b8481f0acbf1c4b4c0cc084
gibi2 <adm.municipal.sp@gmail.com>
2021-02-15 16:15:09 -0300

    Instalado com sucesso na sua máquina: desktop!

--- gitcam.sh
+++ gitcam.sh
@@ -92,3 +92,4 @@ done
 # Instalado em seg fev 15 15:52:13 -03 2021
 # Instalado em seg fev 15 16:05:13 -03 2021
 # Instalado em seg fev 15 16:06:41 -03 2021
+# Instalado em seg fev 15 16:15:09 -03 2021
